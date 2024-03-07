c82105edded312b1fd0e11fc6480983f54775bb8
suzuy <suzuy@DESKTOP-L5O3NGQ>
2024-03-07 23:34:28 +0200

    new

--- Registreerimine_ja_autoriseerimine.py
+++ Registreerimine_ja_autoriseerimine.py
@@ -2,6 +2,7 @@ from module1 import *
 i=[]
 p=[]    
 x=0
+y=0
 print("Welcome to the cmd virtual account system")
 while True:
     print("\n1-Register\n2-Login")
@@ -17,13 +18,29 @@ while True:
     elif o=="2":
         i1=input("Enter your Login")
         if i1==i:
-            p1=("Enter your Password")
+            p1=input("Enter your Password")
             if p1==p:
                 print("You succesfully entered your account")
             else:
-                while p1==p:
+                while p1!=p:
+                    x+=1
                     print("Looks like you forgot your password\nTry again:")
                     p1=input("Repeat your Password:")
                     if x==2:
                         print("Your password confirmation attempts have ended")
                         break
+        else:
+            print("Login not found")
+            
+    elif o == "3":
+        while True:
+            print("1. Change Login\n2. Change Password\n3. Cancel")  
+            choice=input("Choose action: ")       
+            if choice=="1":
+                user=change(user)
+            elif choice=="2":
+                userpass=password(userpass)
+            elif choice=="3":
+                break
+
+            
--- module1.py
+++ module1.py
@@ -19,3 +19,30 @@ def Register(i:list,p:list)->any:
     print("Thanks for Creating your Account!")
     print("Welcome!",i+"!")
     return i,p,l1
+
+def change(login):
+    i=input("Enter new login: ")
+    print("Login successfully changed.")
+    return login
+
+def password(password):
+    p=input("Enter new password: ")
+    print("Password successfully changed.")
+    return password
+
+def main():
+    user="login"
+    userpass="password"
+
+    while True:
+        print("1. Change Login\n2. Change Password\n3. Cancel")  
+        choice=input("Choose action: ")       
+        if choice=="1":
+            user=change(user)
+        elif choice=="2":
+            userpass=password(userpass)
+        elif choice=="3":
+            break
+        else:
+            print("Please enter 1, 2, or 3")
+    
